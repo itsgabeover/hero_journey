7dcf292a9acd0cc8904be260ab01e886b3806a6b
root <root@Moltres-PC>
2025-04-16 14:26:44 -0400

    comma

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
-  key: '_hero_journey_session'
+  key: '_hero_journey_session',
   same_site: :lax, # ✅ best for localhost
   secure: false    # ✅ allow http during development
