0b152073edd1aa82e8bc6b5f04cfbb7dcd34dd3e
root <root@Moltres-PC>
2025-04-16 14:39:30 -0400

    cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
   key: '_hero_journey_session',
   same_site: :none, # ✅ best for localhost
-  secure: false    # ✅ allow http during development
+  secure: true    # ✅ allow http during development
