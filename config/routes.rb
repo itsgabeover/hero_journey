b0d4a3ab9bc1bf6d4df2ab5e097c1d233d773edf
root <root@Moltres-PC>
2025-05-01 13:34:04 -0400

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   resources :journals
   resources :users
   resources :folders, only: [:index, :show, :create, :update, :destroy]
-  resources :quests, only: [:index, :create, :update, :destroy]
-  
+  namespace :api do
+  namespace :v1 do
+    resources :quests
+  end
+end
+
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/me", to: "sessions#show"
